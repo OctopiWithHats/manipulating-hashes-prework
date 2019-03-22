cc382a00b06a33deafdf1264824283873b45dae6
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 21:26:09 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   
-  contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
+  contacts.each do |person, data|
 
 
   #remember to return your newly altered contacts hash!
